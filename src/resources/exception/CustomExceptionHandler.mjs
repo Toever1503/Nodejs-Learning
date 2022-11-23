7ccac0a64c92642b7d7172d1e82291da41100e2d
Toever1503 <Haunv1503@gmail.com>
2022-11-23 14:52:42 +0700

    update custom err

--- src/resources/exception/CustomExceptionHandler.mjs
+++ src/resources/exception/CustomExceptionHandler.mjs
@@ -1,10 +1,13 @@
 import CustomErrorDto from "../../dtos/CustomErrorDto.mjs";
+import AccessDeniedError from "../../configs/security/errors/AccessDeniedError.mjs";
 
 export default function CustomExceptionHandler(err, req, res, next) {
     console.log(err)
     if (err instanceof CustomErrorDto) {
         res.send(err);
-    } else {
-        res.send(new CustomErrorDto(-1));
+    } else if (err instanceof AccessDeniedError)
+        res.status(403).send(err);
+    else {
+        res.status(500).send(new CustomErrorDto(-1));
     }
 }
